ae1af0de14c922c7cb02fdb4206b157129c26eae
Marcos de Mello Wermelinger Bittencourt <marcosmwb1919@gmail.com>
2018-04-18 12:58:34 -0300

    Create Empregado.java

---
+++ introducao-01/Empregado.java
@@ -0,0 +1,37 @@
+public class Empregado 
+{	private String nome;
+	private double salario;
+	
+	//private static String telefone = "22223333";
+
+	// Quando a instancia tem o mesmo nome da classe isso é chamado construtor, Empregado é construtor
+	public Empregado(String nome, double salario)
+	{	this.nome = nome;
+		this.salario = salario;
+	}
+	
+	public void imprime()
+	{	System.out.println("Nome = " + nome + " salário = " + salario);
+	}
+	
+	public String toString()
+	{	return "Nome = " + nome + " salário = " + salario;
+	}
+
+	public static String getTelefone()
+	{	return telefone;
+	}
+
+	public String getNome() 
+	{	return nome;
+	}
+	public void setNome(String nome) 
+	{	this.nome = nome;
+	}
+	public double getSalario() 
+	{	return salario;
+	}
+	public void setSalario(double salario) 
+	{	this.salario = salario;
+	}
+}
